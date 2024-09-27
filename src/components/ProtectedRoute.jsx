ced30a26dbd8ab6eb5e7415779abbb9307ced961
sebuhiiiiii <sebuhiiiiii36@gmail.com>
2024-09-27 16:57:00 +0400

    jwt

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,13 @@
 import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 export default function ProtectedRoute({ children }) {
   const { user } = useAuth();
 
   if (!user) return <Navigate to="/login" replace={true} />;
 
-  return children;
+  const id = jwtDecode(user)?.user_id;
+
+  if (id == `${import.meta.env.VITE_ADMIN_ID}`) return children;
 }
